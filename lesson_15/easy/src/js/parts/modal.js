f90aa6f30c2cfd20d170a4c52256316cb808c80e
Artem Cherepnin <artnerab@gmail.com>
2019-05-21 16:54:01 +0300

    lesson 15

---
+++ lesson_15/easy/src/js/parts/modal.js
@@ -0,0 +1,21 @@
+function modal() {
+  let overlay = document.querySelector('.overlay');
+
+  const bindModal = (overlayStatus, overflowStatus, classListMethod, el) => {
+    if(classListMethod == 'add') isActiveBtn = el;
+    if(!el) el = isActiveBtn;
+    overlay.style.display = overlayStatus;
+    el.classList[classListMethod]('more-splash');
+    document.body.style.overflow = overflowStatus;
+  };
+
+
+  document.body.addEventListener('click', event => {
+    let target = event.target;
+
+    if(target.classList.contains('more') || target.classList.contains('description-btn')) bindModal('block', 'hidden', 'add', target);
+    if(target.classList.contains('popup-close')) bindModal('none', '', 'remove');
+  });
+}
+
+module.exports = modal;
